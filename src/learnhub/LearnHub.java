6c2f2d575f6fa392b0612b4041e3efadb7832961
luthfi55 <luthfi12354@gmail.com>
2022-04-12 15:16:26 +0800

    menambahkan page login

--- src/learnhub/LearnHub.java
+++ src/learnhub/LearnHub.java
@@ -15,7 +15,11 @@ public class LearnHub {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Login lg =  new Login();
+        lg.setVisible(true);
+        lg.pack();
+        lg.setLocationRelativeTo(null);
+        lg.setDefaultCloseOperation(Login.EXIT_ON_CLOSE);
     }
     
 }
